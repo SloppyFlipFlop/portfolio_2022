50b2875cf29725c947b1ebf5edca7f1ec6070078
David Nevarez <david.nevarez03@hotmail.com>
2022-05-20 18:29:42 -0700

    working on desktop navbar

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -15,8 +15,6 @@ import { GiHamburgerMenu } from "react-icons/gi";
 const Navbar = () => {
   const [navbarOpened, setNavbarOpened] = useState(false);
 
-  // need to make it so that if the user is on desktop, the navbar is automatically opened
-
   return (
     <>
       <div className="menu_icons">
@@ -32,40 +30,38 @@ const Navbar = () => {
           />
         )}
       </div>
-      {navbarOpened ? (
-        <ul>
-          <li>
-            <a href="/" className="wrapper">
-              <AiOutlineHome className="icon" />
-              <div className="name">Home</div>
-            </a>
-          </li>
-          <li>
-            <a href="#about" className="wrapper">
-              <AiOutlineUser className="icon" />
-              <div className="name">About</div>
-            </a>
-          </li>
-          <li>
-            <a href="#resume" className="wrapper">
-              <AiOutlineFileText className="icon" />
-              <div className="name">Resume</div>
-            </a>
-          </li>
-          <li>
-            <a href="#projects" className="wrapper">
-              <FiServer className="icon" />
-              <div className="name">Projects</div>
-            </a>
-          </li>
-          <li>
-            <a href="#contact" className="wrapper">
-              <AiOutlineMail className="icon" />
-              <div className="name">Contact</div>
-            </a>
-          </li>
-        </ul>
-      ) : null}
+      <ul className={`desktop ${navbarOpened ? "show" : "hide"}`}>
+        <li>
+          <a href="/" className="wrapper">
+            <AiOutlineHome className="icon" />
+            <div className="name">Home</div>
+          </a>
+        </li>
+        <li>
+          <a href="#about" className="wrapper">
+            <AiOutlineUser className="icon" />
+            <div className="name">About</div>
+          </a>
+        </li>
+        <li>
+          <a href="#resume" className="wrapper">
+            <AiOutlineFileText className="icon" />
+            <div className="name">Resume</div>
+          </a>
+        </li>
+        <li>
+          <a href="#projects" className="wrapper">
+            <FiServer className="icon" />
+            <div className="name">Projects</div>
+          </a>
+        </li>
+        <li>
+          <a href="#contact" className="wrapper">
+            <AiOutlineMail className="icon" />
+            <div className="name">Contact</div>
+          </a>
+        </li>
+      </ul>
     </>
   );
 };
