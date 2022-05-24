c4dd6a007afea7422d78908d3bc79fa7a4765a17
David Nevarez <david.nevarez03@hotmail.com>
2022-05-23 18:28:19 -0700

    starting to work on home page

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./../styles/pages/Home.scss";
 
+import landingImage from "./../styles/util/images/landing-image.jpg";
+
 const Home = () => {
   return (
     <body className="container">
-      <div className="section-wrapper" id="home">Home</div>
+      <div className="section-wrapper" id="home">
+        <img src={landingImage} alt="David's portfilo landing image" />
+      </div>
 
       <div className="section-wrapper" id="about">About</div>
     </body>
